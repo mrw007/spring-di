f27605624cd3bfbf0ff7721c8ca4b9e9a7054fab
Wahib Kerkeni <mr.wahib@gmail.com>
2022-06-26 12:56:47 +0100

    Added Constructor Injected Controller

---
+++ src/main/java/mrw007/springframework/springdi/controllers/ConstructorInjectedController.java
@@ -0,0 +1,17 @@
+package mrw007.springframework.springdi.controllers;
+
+
+import mrw007.springframework.springdi.services.GreetingService;
+
+public class ConstructorInjectedController {
+    private final GreetingService greetingService;
+
+    public ConstructorInjectedController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting(){
+        return greetingService.sayGreeting();
+    }
+
+}
---
+++ src/test/java/mrw007/springframework/springdi/controllers/ConstructorInjectedControllerTest.java
@@ -0,0 +1,20 @@
+package mrw007.springframework.springdi.controllers;
+
+import mrw007.springframework.springdi.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ConstructorInjectedControllerTest {
+
+    ConstructorInjectedController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new ConstructorInjectedController(new GreetingServiceImpl());
+    }
+
+    @Test
+    void getGreeting() {
+        System.out.println(controller.getGreeting());
+    }
+}
