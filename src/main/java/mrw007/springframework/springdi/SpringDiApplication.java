d3c959b04917ba87a2a7bc748a8c9c1a76665ac9
Wahib Kerkeni <mr.wahib@gmail.com>
2022-06-26 12:45:16 +0100

    Initial project Add

---
+++ src/main/java/mrw007/springframework/springdi/SpringDiApplication.java
@@ -0,0 +1,13 @@
+package mrw007.springframework.springdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDiApplication.class, args);
+	}
+
+}
