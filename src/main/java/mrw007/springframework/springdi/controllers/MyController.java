15e08e6b71a6c6fb3eeca53041fe919fd584ed03
Wahib Kerkeni <mr.wahib@gmail.com>
2022-06-26 14:09:58 +0100

    Added Primary Service

--- src/main/java/mrw007/springframework/springdi/controllers/MyController.java
+++ src/main/java/mrw007/springframework/springdi/controllers/MyController.java
@@ -1,11 +1,18 @@
 package mrw007.springframework.springdi.controllers;
 
+import mrw007.springframework.springdi.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String sayHello(){
-        System.out.println("Hello World!!");
-        return "Hi Folks";
+        return greetingService.sayGreeting();
     }
 }
