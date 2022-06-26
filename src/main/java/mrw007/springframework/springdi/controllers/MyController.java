dc5cb678bcb6f430cb6df64808e1881846244448
Wahib Kerkeni <mr.wahib@gmail.com>
2022-06-26 12:51:53 +0100

    Added basic controller

---
+++ src/main/java/mrw007/springframework/springdi/controllers/MyController.java
@@ -0,0 +1,11 @@
+package mrw007.springframework.springdi.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public String sayHello(){
+        System.out.println("Hello World!!");
+        return "Hi Folks";
+    }
+}
